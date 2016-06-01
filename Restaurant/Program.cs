00ca9f5bf63d832ae3a1092cce6afd14dd161919
Ebbe Flarup <ebbeflarup@gmail.com>
2016-06-01 16:20:38 +0200

    changes

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -8,10 +8,12 @@ namespace Restaurant
         {
             var waitor = new Waitor(new Cook(new AssistantManager(new Cashier(new OrderPrinter()))));
 
-            var lineItems = new LineItemList();
-            lineItems.Add(new LineItem(2, "Ice Cream", 2.99));
-            lineItems.Add(new LineItem(2, "Burger", 4.99));
-            waitor.PlaceOrder(2, new LineItemList());
+            var lineItems = new LineItemList
+            {
+                new LineItem(2, "Ice Cream", 2.99),
+                new LineItem(2, "Burger", 4.99)
+            };
+            waitor.PlaceOrder(2, lineItems);
         }
     }
 }
