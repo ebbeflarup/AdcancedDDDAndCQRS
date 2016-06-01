510c2cb72fc89d5a21244a62fc6499f2b038574e
Ebbe Flarup <ebbeflarup@gmail.com>
2016-06-01 16:13:42 +0200

    program

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Restaurant
+﻿using Newtonsoft.Json.Linq;
+
+namespace Restaurant
 {
     class Program
     {
         static void Main(string[] args)
         {
             var waitor = new Waitor(new Cook(new AssistantManager(new Cashier(new OrderPrinter()))));
+
+            var lineItems = new LineItemList();
+            lineItems.Add(new LineItem(2, "Ice Cream", 2.99));
+            lineItems.Add(new LineItem(2, "Burger", 4.99));
+            waitor.PlaceOrder(2, new LineItemList());
         }
     }
 }
