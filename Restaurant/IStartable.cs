ec56b9a5eff0193b6b5a1f38efd9c90a44601ab7
Rune Fogh <rfogh@rfogh.dk>
2016-06-02 08:58:31 +0200

    Added ThreadedHandler

---
+++ Restaurant/IStartable.cs
@@ -0,0 +1,7 @@
+﻿namespace Restaurant
+{
+    public interface IStartable
+    {
+        void Start();
+    }
+}
---
+++ Restaurant/ThreadedHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Restaurant
+{
+    public class ThreadedHandler : IHandleOrder, IStartable
+    {
+        private readonly IHandleOrder _handleOrder;
+        private readonly ConcurrentQueue<Order> _orders; 
+
+        public ThreadedHandler(IHandleOrder handleOrder)
+        {
+            _handleOrder = handleOrder;
+        }
+
+        public void Start()
+        {
+            var thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    Order order;
+                    while (_orders.TryDequeue(out order))
+                    {
+                        _handleOrder.Handle(order);
+                    }
+                    Thread.Sleep(1);
+                }
+            });
+            thread.Start();
+        }
+
+        public void Handle(Order order)
+        {
+            _orders.Enqueue(order);
+        }
+    }
+}
