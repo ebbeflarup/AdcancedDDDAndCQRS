a207740041904eaba11ead503eb2179ea4ccd007
Ebbe Flarup <ebbeflarup@gmail.com>
2016-06-02 13:12:06 +0200

    message base

---
+++ Messages/MessageBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Messages
+{
+    public class MessageBase
+    {
+        public MessageBase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
