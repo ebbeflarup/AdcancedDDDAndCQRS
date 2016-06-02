ac22bb011298456690702541dfad34f8ca6d976a
Rune Fogh <rfogh@rfogh.dk>
2016-06-02 08:13:29 +0200

    Added Multiplexer

---
+++ Restaurant/Multiplexer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Restaurant
+{
+    public class Multiplexer : IHandleOrder
+    {
+        private readonly IEnumerable<IHandleOrder> _handleOrders;
+        public Multiplexer(IEnumerable<IHandleOrder> handleOrders)
+        {
+            _handleOrders = handleOrders;
+        }
+
+        public void Handle(Order order)
+        {
+            foreach (var handleOrder in _handleOrders)
+            {
+                handleOrder.Handle(order);
+            }
+        }
+    }
+}
