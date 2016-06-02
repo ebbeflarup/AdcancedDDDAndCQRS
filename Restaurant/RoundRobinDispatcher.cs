88e4def2b65668527e6518fe1ceb881e48d5e6a2
Rune Fogh <rfogh@rfogh.dk>
2016-06-02 08:28:45 +0200

    Added RoundrobinDispatcher

---
+++ Restaurant/RoundRobinDispatcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Restaurant
+{
+    public class RoundRobinDispatcher : IHandleOrder
+    {
+        private readonly Queue<IHandleOrder> _handleOrders;
+
+        public RoundRobinDispatcher(IEnumerable<IHandleOrder> handleOrders)
+        {
+            _handleOrders = new Queue<IHandleOrder>(handleOrders);
+        }
+
+        public void Handle(Order order)
+        {
+            _handleOrders.Peek().Handle(order);
+
+            var handleOrder = _handleOrders.Dequeue();
+            _handleOrders.Enqueue(handleOrder);
+        }
+    }
+}
