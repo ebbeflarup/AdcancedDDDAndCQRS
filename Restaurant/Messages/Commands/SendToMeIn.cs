ac802e0f8c6c4cf92d105e48ca97bf628803c9cd
Ebbe Flarup <ebbeflarup@gmail.com>
2016-06-03 14:07:37 +0200

    sendtomein

--- Restaurant/Messages/Commands/SendToMeIn.cs
+++ Restaurant/Messages/Commands/SendToMeIn.cs
@@ -2,9 +2,13 @@
 {
     public class SendToMeIn<TMessage>
     {
+        public int Seconds { get; }
+        public TMessage InnerMessage { get; }
+
         public SendToMeIn(int seconds, TMessage message)
         {
-            
+            Seconds = seconds;
+            InnerMessage = message;
         }
     }
 }
