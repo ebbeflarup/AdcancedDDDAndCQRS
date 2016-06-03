0bec784c16bf967a056568d6ac61f8e77ffd4221
Ebbe Flarup <ebbeflarup@gmail.com>
2016-06-03 14:28:14 +0200

    order process manager now retries cooking

--- Restaurant/ProcessManager/OrderProcessManager.cs
+++ Restaurant/ProcessManager/OrderProcessManager.cs
@@ -4,26 +4,30 @@ using Restaurant.Messages.Events;
 
 namespace Restaurant.ProcessManager
 {
-    public class OrderProcessManager : IOrderProcessManager, IHandle<OrderCooked>, IHandle<OrderPriced>, IHandle<OrderPaid>
+    public class OrderProcessManager : IOrderProcessManager, IHandle<OrderCooked>, IHandle<OrderPriced>, IHandle<OrderPaid>, IHandle<RetryCookFood>
     {
         private readonly IPublisher _publisher;
+        private bool _isFoodCooked;
 
         public Action<Guid> OnCompleted { get; set; }
 
         public OrderProcessManager(IPublisher publisher)
         {
             _publisher = publisher;
+            _isFoodCooked = false;
         }
 
         public void Handle(OrderPlaced orderPlaced)
         {
             Console.WriteLine("OrderPlaced");
             _publisher.Publish(new CookFood(orderPlaced.Order, orderPlaced.CorrelationId, orderPlaced.Id));
+            _publisher.Publish(new SendToMeIn(10, new RetryCookFood(orderPlaced.Order, orderPlaced.CorrelationId, orderPlaced.Id), orderPlaced.CorrelationId, orderPlaced.CausationId));
         }
 
         public void Handle(OrderCooked orderCooked)
         {
             Console.WriteLine("OrderCooked");
+            _isFoodCooked = true;
             _publisher.Publish(new PriceOrder(orderCooked.Order, orderCooked.CorrelationId, orderCooked.Id));
         }
 
@@ -37,6 +41,14 @@ namespace Restaurant.ProcessManager
         {
             Console.WriteLine("OrderPaid");
             OnCompleted(orderPaid.CorrelationId);
+        }
+
+        public void Handle(RetryCookFood orderPlaced)
+        {
+            if (_isFoodCooked)
+                return;
+
+            _publisher.Publish(new CookFood(orderPlaced.Order, orderPlaced.CorrelationId, orderPlaced.CausationId));
         }
     }
 }
--- Restaurant/ProcessManager/PaymentFirstOrderProcessmanager.cs
+++ Restaurant/ProcessManager/PaymentFirstOrderProcessmanager.cs
@@ -4,15 +4,18 @@ using Restaurant.Messages.Events;
 
 namespace Restaurant.ProcessManager
 {
-    public class PaymentFirstOrderProcessmanager : IOrderProcessManager, IHandle<OrderCooked>, IHandle<OrderPriced>, IHandle<OrderPaid>
+    public class PaymentFirstOrderProcessmanager : IOrderProcessManager, IHandle<OrderCooked>, IHandle<OrderPriced>, IHandle<OrderPaid>, IHandle<RetryCookFood>
     {
         private readonly IPublisher _publisher;
 
         public Action<Guid> OnCompleted { get; set; }
 
+        private bool _isFoodCooked;
+
         public PaymentFirstOrderProcessmanager(IPublisher publisher)
         {
             _publisher = publisher;
+            _isFoodCooked = false;
         }
 
         public void Handle(OrderPlaced orderPlaced)
@@ -24,6 +27,7 @@ namespace Restaurant.ProcessManager
         public void Handle(OrderCooked orderCooked)
         {
             Console.WriteLine("OrderCooked");
+            _isFoodCooked = true;
             OnCompleted(orderCooked.CorrelationId);
         }
 
@@ -37,6 +41,15 @@ namespace Restaurant.ProcessManager
         {
             Console.WriteLine("OrderPaid");
             _publisher.Publish(new CookFood(orderPaid.Order, orderPaid.CorrelationId, orderPaid.Id));
+            _publisher.Publish(new SendToMeIn(10, new RetryCookFood(orderPaid.Order, orderPaid.CorrelationId, orderPaid.Id), orderPaid.CorrelationId, orderPaid.CausationId));
+        }
+
+        public void Handle(RetryCookFood orderPlaced)
+        {
+            if (_isFoodCooked)
+                return;
+
+            _publisher.Publish(new CookFood(orderPlaced.Order, orderPlaced.CorrelationId, orderPlaced.CausationId));
         }
     }
 }
