b25f42e888d5b35cde005625286030af80960323
Ebbe Flarup <ebbeflarup@gmail.com>
2016-06-03 12:43:26 +0200

    pay first order process manager added

---
+++ Restaurant/ProcessManager/PaymentFirstOrderProcessmanager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Restaurant.Messages.Commands;
+using Restaurant.Messages.Events;
+
+namespace Restaurant.ProcessManager
+{
+    public class PaymentFirstOrderProcessmanager : IHandle<OrderPlaced>, IHandle<OrderCooked>, IHandle<OrderPriced>, IHandle<OrderPaid>
+    {
+        private readonly IPublisher _publisher;
+
+        public Action<Guid> OnCompleted;
+
+        public PaymentFirstOrderProcessmanager(IPublisher publisher)
+        {
+            _publisher = publisher;
+        }
+
+        public void Handle(OrderPlaced orderPlaced)
+        {
+            Console.WriteLine("OrderPlaced");
+            _publisher.Publish(new PriceOrder(orderPlaced.Order, orderPlaced.CorrelationId, orderPlaced.Id));
+        }
+
+        public void Handle(OrderCooked orderCooked)
+        {
+            Console.WriteLine("OrderCooked");
+            OnCompleted(orderCooked.CorrelationId);
+        }
+
+        public void Handle(OrderPriced orderPriced)
+        {
+            Console.WriteLine("OrderPriced");
+            _publisher.Publish(new TakePayment(orderPriced.Order, orderPriced.CorrelationId, orderPriced.Id));
+        }
+
+        public void Handle(OrderPaid orderPaid)
+        {
+            Console.WriteLine("OrderPaid");
+            _publisher.Publish(new CookFood(orderPaid.Order, orderPaid.CorrelationId, orderPaid.Id));
+        }
+    }
+}
