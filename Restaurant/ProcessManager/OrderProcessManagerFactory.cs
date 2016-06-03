01da24824d891eac2111302485ecf47cbac0c6c7
Ebbe Flarup <ebbeflarup@gmail.com>
2016-06-03 13:12:52 +0200

    added missing file

---
+++ Restaurant/ProcessManager/OrderProcessManagerFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace Restaurant.ProcessManager
+{
+    public class OrderProcessManagerFactory
+    {
+        private readonly IPublisher _publisher;
+        public OrderProcessManagerFactory(IPublisher publisher)
+        {
+            _publisher = publisher;
+        }
+
+        public IOrderProcessManager CreateOrderProcessManager(Order order)
+        {
+            if (order.IsDodgy)
+                return new PaymentFirstOrderProcessmanager(_publisher);
+
+            return new OrderProcessManager(_publisher);
+        }
+    }
+}
