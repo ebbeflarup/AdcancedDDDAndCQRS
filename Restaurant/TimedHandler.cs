4908be8827735e3f3b095cd49cacf572fb75d363
Rune Fogh <rfogh@rfogh.dk>
2016-06-03 14:35:43 +0200

    cleanup

--- Restaurant/TimedHandler.cs
+++ Restaurant/TimedHandler.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using Restaurant.Messages;
 using Restaurant.Messages.Commands;
 
 namespace Restaurant
